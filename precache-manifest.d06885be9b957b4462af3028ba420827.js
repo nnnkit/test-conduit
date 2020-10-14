9b1bcb468f889a9c9760aee264183751ed463e37
Ankit <iankit17@gmail.com>
2020-10-14 13:39:16 +0530

    Updates

---
+++ precache-manifest.d06885be9b957b4462af3028ba420827.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8274d3b2539d15bcd4c4665938e3f533",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad180b077b3379ee87de",
+    "url": "/static/css/main.42e0ee90.chunk.css"
+  },
+  {
+    "revision": "ef03982eadf39ec2083f",
+    "url": "/static/js/2.370c56df.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.370c56df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad180b077b3379ee87de",
+    "url": "/static/js/main.3ab8ea8c.chunk.js"
+  },
+  {
+    "revision": "95ab83a4221008eaadda",
+    "url": "/static/js/runtime-main.1dd9f351.js"
+  }
+]);
